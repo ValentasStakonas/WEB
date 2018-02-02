8258d84030bf3b6f6b33e80a6190018ec2783bc1
unknown <valentas.stakonas@gmail.com>
2018-02-02 18:24:13 +0200

    init

---
+++ class work/namu darbai/php/index.php
@@ -0,0 +1,12 @@
+<?php
+$x = "<h1>Vaisių sąrašas</h1>";
+$y = '<ul>
+        <li>Obuoliai</li>
+        <li>Bananai</li>
+        <li>Kriaušės</li>
+        <li>Apelsinais</li>
+        <li>Mandarinai</li>
+      </ul>';
+echo $x;
+echo $y;
+?>
